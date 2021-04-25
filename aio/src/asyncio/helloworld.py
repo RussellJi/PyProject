3be51d74c69ed2bb6f5f79a504baecdfff40f8b0
RussellJi <17361564789@163.com>
2021-04-25 18:39:18 +0800

    aio test

---
+++ aio/src/asyncio/helloworld.py
@@ -0,0 +1,13 @@
+import asyncio
+
+@asyncio.coroutine
+def hello():
+    print("hello world")
+    r = yield from asyncio.sleep(1)
+    print("hello again")
+
+# 获取eventloop:
+loop = asyncio.get_event_loop()
+# 执行coroutine
+loop.run_until_complete(hello())
+loop.close()
